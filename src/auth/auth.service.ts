b5fdf05142003f35e24d6939c952ff3953e840af
Martimex <kamilmarciniak533@wp.pl>
2026-02-20 21:55:57 +0100

    chore: initialize repository with initial files
    Added baseline for the Nest project - with three
    initial Modules (User, Bookmark, Auth). The
    current state of the project does not contain
    any ORM or database yet. Those are yet to be
    added in the future commit.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+
+    signin() {
+        return {msg: "I am signed in"};
+    }
+
+    signup() {
+        return {msg: "I am signed up"};
+    }
+}
